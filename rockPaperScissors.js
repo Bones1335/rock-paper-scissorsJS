28441b20ec6eebc6772b98031529468e83715766
Bones <ahansen1335@gmail.com>
2021-12-24 10:12:27 +0100

    pseusocode for R, P, S game play loop

---
+++ rockPaperScissors.js
@@ -0,0 +1,22 @@
+//make variable for 'Rock'
+//make variable for 'Paper'
+//make variable for 'Scissors'
+//make computerPlay function that randomly returns 'Rock', 'Paper', or 'Scissors
+    //make variable choosing randomly between R, P, and S
+    //return computer result in variable computerSelection
+//make playRound function with playerSelection, computerSelection as the two parameters
+    //make playerSelection variable
+    //make computerSelection variable calling computerPlay function
+    //if playerSelection === 'rock' && computerSelection === 'rock' => tie
+    //else if playerSelection === 'rock' && computerSelection === 'paper' => comp wins
+    //else if playerSelection === 'rock' && computerSelecion === 'scissors' => player wins
+    //else if playerSelection === 'paper' && computerSelection === 'rock' => player wins
+    //else if playerSelection === 'paper' && computerSelection === 'paper' => tie
+    //else if playerSelection === 'paper' && computerSelection === 'scissors' => comp wins
+    //else if playerSelection === 'scissors' && computerSelection === 'rock' => comp wins
+    //else if playerSelection === 'scissors' && computerSelection === 'paper' => player wins
+    //else if playerSelection === 'scissors' && computerSelection === 'scissors' => tie
+    //return results
+//make game function looping through 5 rounds of play
+    //call playRound function 5 times
+    //return wins, losses, ties
