6d41390ea112a57f0b6bbc6ef2b99bfb9d8cec56
Bones <ahansen1335@gmail.com>
2021-12-24 15:38:02 +0100

    returning "Not working" when playRound called. Need to fix later

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,10 +1,8 @@
-//const rock = 0; //make variable for 'Rock'
-//const paper = 1; //make variable for 'Paper'
-//const scissors = 2; //make variable for 'Scissors'
-
-function randomInt(min, max) { //make function choosing randomly between R, P, and S
+//make function choosing randomly between R, P, and S
+function randomInt(min, max) { 
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
 //make computerPlay function that randomly returns 'Rock', 'Paper', or 'Scissors'
 function computerPlay() {
     const computer = randomInt(0, 3);
@@ -17,19 +15,38 @@ function computerPlay() {
     } else
         return console.log('Not working')
 }
+
+//make playerSelection variable
+const playerSelection = prompt('Please choose Rock, Paper, or Scissors: ');
+
+
 //make playRound function with playerSelection, computerSelection as the two parameters
-    //make playerSelection variable
+function playRound(playerSelection, computerSelection) {
     //make computerSelection variable calling computerPlay function
-    //if playerSelection === 'rock' && computerSelection === 'rock' => tie
-    //else if playerSelection === 'rock' && computerSelection === 'paper' => comp wins
-    //else if playerSelection === 'rock' && computerSelecion === 'scissors' => player wins
-    //else if playerSelection === 'paper' && computerSelection === 'rock' => player wins
-    //else if playerSelection === 'paper' && computerSelection === 'paper' => tie
-    //else if playerSelection === 'paper' && computerSelection === 'scissors' => comp wins
-    //else if playerSelection === 'scissors' && computerSelection === 'rock' => comp wins
-    //else if playerSelection === 'scissors' && computerSelection === 'paper' => player wins
-    //else if playerSelection === 'scissors' && computerSelection === 'scissors' => tie
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return console.log('It\'s a tie!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return console.log('Computer wins. Paper beats Rock.');
+    } else if (playerSelection === 'rock' && computerSelecion === 'scissors') {
+        return console.log('You win! Rock beats Scissors!');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return console.log('You win! Paper beats Rock!');
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return console.log('It\'s a tie!');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return console.log('Computer wins. Scissors beat Paper.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return console.log('Computer wins. Rock beats scissors.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return console.log('You win! Scissors beat Paper!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return console.log('It\s a tie!');
+    } else 
+        return console.log('Not working.');
     //return results
+
+}
+console.log(playRound(playerSelection, computerPlay()))
 //make game function looping through 5 rounds of play
     //call playRound function 5 times
     //return wins, losses, ties
